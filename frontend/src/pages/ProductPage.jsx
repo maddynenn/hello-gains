9e8ca73dd236eb9fb2acbf2b83bc855d1a429dc4
cooper-ke <cooper.ke@northeastern.edu>
2025-10-04 14:51:04 -0400

    Added example products

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,14 +1,44 @@
 import React from "react";
 import "../style/ProductPage.css";
 import shoe from "../assets/Owen.png";
+import chocolateShake from "../assets/chocolate.jpeg";
+import nuttyDrink from "../assets/nuts.jpg";
+import sodas from "../assets/sodas.webp";
+import strawberryShake from "../assets/strawberry.jpeg";
 
 export default function ProductPage() {
-  const products = Array(10).fill({
-    name: "Owen Sharpe",
-    quantity: 3,
-    total: 50,
-    image: shoe,
-  });
+    const products = [
+    {
+      name: "Owen Sharpe",
+      quantity: 3,
+      total: 50,
+      image: shoe,
+    },
+    {
+      name: "Chocolate Box",
+      quantity: 12,
+      total: 12,
+      image: chocolateShake,
+    },
+    {
+      name: "Nutmeg Box",
+      quantity: 7,
+      total: 15,
+      image: nuttyDrink,
+    },
+    {
+      name: "Carbonation Vacation",
+      quantity: 4,
+      total: 3,
+      image: sodas,
+    },
+    {
+      name: "Strawberry Blast",
+      quantity: 3,
+      total: 10,
+      image: strawberryShake,
+    }
+  ];
 
   return (
     <div className="product-page">
